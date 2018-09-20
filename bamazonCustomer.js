abb17599db89a90ca72a6cb452aabf80b3c1f5aa
Muddy Lee <muddy@Muddys-iMac.local>
2018-09-19 23:22:10 -0700

    got promises working!  Both BamazonCustomer and BamazonManager are up and working.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,13 +2,15 @@ var mysql = require('mysql');
 const cTable = require('console.table');
 var inquirer = require('inquirer');
 var keys = require('./keys.js');
+var Promise = require('promise');
+var connection = mysql.createConnection(keys);
 
 
-function selectAllProducts(){
-    connection.query('SELECT * FROM products', function(err, resp){
+function selectAllProducts() {
+    connection.query('SELECT * FROM products', function (err, resp) {
         if (err) throw err;
         var values = [];
-        for (var i = 0; i < resp.length; i++){
+        for (var i = 0; i < resp.length; i++) {
             var value = [resp[i].item_id, resp[i].product_name, resp[i].department_name, `$${resp[i].price}`, resp[i].stock_quantity];
 
             values.push(value);
@@ -19,57 +21,119 @@ function selectAllProducts(){
     })
 }
 
-var connection = mysql.createConnection(keys)
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
-    console.log('connected as id ' + connection.threadId);
-    selectAllProducts();
-
+    console.log('Welcome to Bamazon.com!!');
+    menu();
 })
 
-start();
 
-function start(){
-   
+
+async function start() {
+    await viewProductsPromise();
 
     inquirer
-    .prompt([{
-        name: "selectID",
-        type: 'input',
-        message: 'Please select the product id you would like to purchase'
-    },
-    {   name: 'selectQty',
-        type: 'input',
-        message: 'Quanity to purchase:'
-    }])
-    .then(function(answer){
-        var selectionQty = answer.selectQty;
-        var selectionID = answer.selectID;
-        connection.query('SELECT * FROM products', function(err, resp){
+        .prompt([{
+                name: "selectID",
+                type: 'input',
+                message: 'Please select the product id you would like to purchase'
+            },
+            {
+                name: 'selectQty',
+                type: 'input',
+                message: 'Quanity to purchase:'
+            }
+        ])
+        .then(function (answer) {
+            let productID = answer.selectID;
+            var purchaseQTY = parseInt(answer.selectQty);
 
-            if (err) throw err;
-            console.log(selectionQty + "<-- quantity selected");
-            console.log(selectionID + "<--selectionID");
-            console.log(resp[selectionID-1].stock_quantity+ "<--resp selection stock quantity");
-            if(selectionQty <= resp[selectionID-1].stock_quantity){
-                //update stock quantity
-                connection.query("UPDATE products SET ? WHERE ?",
-                    [
-                        { stock_quantity: (resp[selectionID-1].stock_quantity - answer.selectQty)},
-                    { item_id: answer.selectID} 
-                    ])
-                console.table([
-                    {Amount: `${answer.selectQty} qty.`,
-                    Product: resp[selection - 1].product_name,
-                    Price: `$${resp[selection - 1].price}`,Total: `$${resp[selection-1].price*answer.selectQty}`}
-                ])
-            } else{
-                console.log("Insufficient quantity!")
+            connection.query('SELECT * FROM products', function (err, resp) {
+
+                if (err) throw err;
+                console.log(purchaseQTY + "<-- quantity selected");
+                console.log(typeof (purchaseQTY) + "<- typeof purchaseQTY")
+                console.log(productID + "<--productID");
+                let stockQTY = resp[productID - 1].stock_quantity;
+                console.log(typeof (stockQTY) + "<--type of stockQTY");
+                console.log(stockQTY + "<--resp selection stock quantity");
+                if (purchaseQTY <= stockQTY) {
+                    //update stock quantity
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [{
+                                stock_quantity: (stockQTY - purchaseQTY)
+                            },
+                            {
+                                item_id: answer.selectID
+                            }
+                        ])
+                    console.log(" *** Your Receipt *** ")
+                    console.table([{
+                        Amount: `${answer.selectQty} qty.`,
+                        Product: `${resp[productID - 1].product_name}`,
+                        Price: `$${resp[productID - 1].price}`,
+                        Total: `$${resp[productID-1].price*purchaseQTY}`
+                    }])
+                    menu();
+                } else {
+                    console.log("Insufficient quantity!")
+
+                }
+            })
+
+
+            menu();
+        })
+}
+
+function menu() {
+    inquirer
+        .prompt([{
+            name: 'menuOption',
+            type: 'rawlist',
+            message: 'Choose from the options below',
+            choices: ['viewProducts', 'makePurchase', 'quit']
+        }])
+        .then(function (answer) {
+            let choice = answer.menuOption;
+            switch (choice) {
+                case 'viewProducts':
+                    startOver();
+                    break;
+                case 'makePurchase':
+                    start();
+                    break;
+                case 'quit':
+                    console.log("Goodbye!");
             }
         })
-        
+}
 
+function viewProductsPromise() {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM products', function (err, resp) {
+            if (err) throw err;
+            var values = [];
+            for (var i = 0; i < resp.length; i++) {
+                var record = resp[i];
+                var value = [
+                    record.item_id,
+                    record.product_name,
+                    record.department_name,
+                    `\$${record.price}`,
+                    record.stock_quantity
+                ];
+                values.push(value);
+            }
+            console.table(['ID', 'Product', 'Category', 'Price', 'Stock'], values);
+            resolve(values);
 
-    })
+        })
+    });
+}
+
+async function startOver() {
+    await viewProductsPromise();
+    menu();
 }
--- bamazonManager.js
+++ bamazonManager.js
@@ -2,8 +2,9 @@ var mysql = require('mysql');
 const cTable = require('console.table');
 var inquirer = require('inquirer');
 var keys = require('./keys.js');
-
+var Promise = require('promise');
 var connection = mysql.createConnection(keys);
+
 
 connection.connect(function (err) {
   if (err) throw err;
@@ -22,66 +23,79 @@ function start() {
     .then(function (answer) {
       var choice = answer.selectOption;
       switch (choice) {
+        case 'Products for Sale':
+          console.log('Products for Sale');
+          //view products for sale
+          startOver();
+          break;
+
         case 'View Low Inventory':
           console.log('View Low Inventory');
           //view low inventory
           viewLow();
           break;
 
-        case 'Products for Sale':
-          console.log('Products for Sale');
-          //view products for sale
-          viewProducts();
-          break;
-
         case 'Update Inventory':
-          console.log('Update Inventory - add stuff, update stuff, etc...');
-          //new prompt to allow for stuff to happen.
+          console.log("Update Inventory");
           updateInventory();
       }
     })
 }
 
+function viewProductsPromise() {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM products', function (err, resp) {
+      if (err) throw err;
+      var values = [];
+      for (var i = 0; i < resp.length; i++) {
+        var record = resp[i];
+        var value = [
+          record.item_id,
+          record.product_name,
+          record.department_name,
+          `\$${record.price}`,
+          record.stock_quantity
+        ];
+        values.push(value);
+      }
+      console.table(['ID', 'Product', 'Category', 'Price', 'Stock'], values);
+      resolve(values);
 
-function viewProducts() {
-  connection.query('SELECT * FROM products', function (err, resp) {
-    if (err) throw err;
-    var values = [];
-    for (var i = 0; i < resp.length; i++) {
-      var record = resp[i];
-      var value = [record.item_id, record.product_name, record.department_name, `$${record.price}`, record.stock_quantity];
-      values.push(value);
-    }
-    console.table(['ID', 'Product', 'Category', 'Price', 'Stock'], values);
-  })
+    })
+  });
 }
 
 
 function viewLow() {
-  connection.query('SELECT * FROM products', function (err, resp) {
-    if (err) throw err;
-    var values = [];
-    for (var i = 0; i < resp.length; i++) {
-      var record = resp[i];
-      if (record.stock_quantity < 6) {
-        var value = [record.item_id, record.product_name, record.department_name, `$${record.price}`, record.stock_quantity];
-        values.push(value);
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM products', function (err, resp) {
+      if (err) throw err;
+      var values = [];
+      for (var i = 0; i < resp.length; i++) {
+        var record = resp[i];
+        if (record.stock_quantity < 6) {
+          var value = [record.item_id, record.product_name, record.department_name, `$${record.price}`, record.stock_quantity];
+          values.push(value);
+        }
       }
-    }
-    console.log("________Low Inventory________");
-    console.table(['ID', 'Product', 'Category', 'Price', 'Stock'], values);
+      console.log("________Low Inventory________");
+      console.table(['ID', 'Product', 'Category', 'Price', 'Stock'], values);
+      resolve(values);
+      start();
+    })
   })
 }
+
 function updateInventory() {
   inquirer
     .prompt([{
-      name: 'updateInventory',
+      name: 'updateInvent',
       type: 'rawlist',
       message: 'Update Inventory',
       choices: ['Add To Stock Quantity', 'Add New Product', 'Remove Product']
     }])
     .then(function (answer) {
-      var choice = answer.updateInventory;
+      var choice = answer.updateInvent;
       switch (choice) {
         case 'Add To Stock Quantity':
           console.log('Add To Stock Quantity');
@@ -91,52 +105,114 @@ function updateInventory() {
           console.log("Add New Product");
           addNewProduct();
           break;
+        case 'Remove Product':
+          console.log('Remove Product');
+          removeProduct();
+          break;
       }
     })
 }
 
-function addToStock() {
-  
+async function addToStock() {
 
-  let promise = new Promise(function(resolve, reject){
-    // viewProducts();
-    resolve();
-  });
+  await viewProductsPromise();
+  inquirer.prompt([{
+      name: "selectID",
+      type: 'input',
+      message: 'Select product id you would like to update quantity of'
+    },
+    {
+      name: 'selectQty',
+      type: 'input',
+      message: 'Quanity to add to stock'
+    }
+  ]).then(function (answer) {
+    var item = answer.selectID;
+    var itemDB = "";
+    var quantityDB;
+    var quantity = answer.selectQty;
+    connection.query('SELECT * FROM products', function (err, resp) {
+      if (err) throw err;
 
-  promise
-  .then(function () {
-    viewProducts();
-    inquirer
-        .prompt([{
-          name: "selectID",
-          type: 'input',
-          message: 'Select product id you would like to update quantity of'
+      itemDB = resp[item - 1].item_id;
+
+      quantityDB = resp[item - 1].stock_quantity;
+
+      connection.query('UPDATE products SET ? WHERE ?', [{
+          stock_quantity: (quantityDB + parseInt(quantity))
         },
         {
-          name: 'selectQty',
-          type: 'input',
-          message: 'Quanity to add to stock'
-        }])
-        .then(function (answer) {
-          var item = answer.selectID;
-          var itemDB = "";
-          var quantityDB;
-          var quantity = answer.selectQty;
-
-          connection.query('SELECT * FROM products', function (err, resp) {
-            if (err) throw err;
-            itemDB = resp[item - 1].item_id;
-            quantityDB = resp[item - 1].stock_quantity;
-          });
-
-          connection.query('UPDATE products SET ? WHERE ?',
-            [
-              { stock_quantity: quantityDB + quantity },
-              { item_id: itemDB }
-            ])
-        })
-    // }
-  // )
+          item_id: itemDB
+        }
+      ]);
+      viewProductsPromise();
+      startOver();
+    })
   })
+}
 
-    }
+function addNewProduct() {
+  inquirer
+    .prompt([{
+        name: 'NameofProduct',
+        type: 'input',
+        message: "Name of Product"
+      },
+      {
+        name: 'Price',
+        type: 'input',
+        message: "Price of Product"
+
+      },
+      {
+        name: 'Department',
+        type: 'input',
+        message: "Department",
+      },
+      {
+        name: 'Quantity',
+        type: 'input',
+        message: 'Initial Quantity'
+      }
+    ]).then(function (answer) {
+      var name = answer.NameofProduct;
+      var dept = answer.Department;
+      var price = parseFloat(answer.Price);
+      var quantity = parseInt(answer.Quantity);
+      connection.query('SELECT * FROM products', function (err, resp) {
+        if (err) throw err;
+        var newRow = `INSERT INTO products(product_name, department_name, price, stock_quantity)
+      VALUES ('${name}', '${dept}', ${price}, ${quantity})`
+        connection.query(newRow, function (err, result) {
+          if (err) throw err;
+          console.log("******* New Product Entered *******");
+          start();
+        })
+      })
+
+    })
+}
+
+async function removeProduct(){
+  await viewProductsPromise();
+  inquirer
+  .prompt([
+    {
+    name: "RemoveProduct",
+    type: 'input',
+    message: "Select ID of product to remove"
+  }])
+  .then(function(answer){
+    let id = answer.RemoveProduct;
+    let sql = 'DELETE FROM products WHERE item_id = ?';
+    connection.query(sql, id, (err, results, fields) => {
+      console.log('Deleted Row:', results.affectedRows);
+      start();
+    })
+  })
+}
+
+async function startOver() {
+  await viewProductsPromise();
+  start();
+}
